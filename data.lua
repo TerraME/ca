074cb4b06da4695518b619d2786fdd1dae7c6ea9
Pedro Andrade <pedro.andrade@inpe.br>
2015-09-21 12:15:46 -0300

    Documentation of data with images.

--- data.lua
+++ data.lua
@@ -1,79 +1,91 @@
 
 data{
 	file = "pentaDecathlon.life",
+	image = "pentaDecathlon.bmp",
 	summary = "Pentadecathlon (or PD) is a period 15 oscillator that was found in 1970 by John Conway while tracking the history of short rows of cells (see one cell thick pattern).",
 	source = "http://www.conwaylife.com/wiki/Pentadecathlon"
 }
 
 data{
 	file = "rPentomino.life",
+	image = "rPentomino.bmp",
 	summary = "The R-pentomino is a methuselah that was found by John Conway in 1970. It is by far the most active polyomino with fewer than six cells; all of the others stabilize in at most 10 generations, but the R-pentomino does not do so until generation 1103, by which time it has a population of 116. ",
 	source = "http://www.conwaylife.com/wiki/R-pentomino"
 }
 
 data{
 	file = "dinnerTable.life",
+	image = "dinnerTable.bmp",
 	summary = "http://www.conwaylife.com/wiki/Dinner_table",
 	source = "Dinner table is a period 12 oscillator. It was the first period 12 oscillator to be found, and was discovered by Robert Wainwright in 1972. It consists of four eater 1s hassling a pre-beehive."
 }
 
 data{
 	file = "figureEight.life",
+	image = "figureEight.bmp",
 	summary = "Figure eight (or less frequently, big beacon) is a period 8 oscillator found by Simon Norton in 1970.",
 	source = "http://www.conwaylife.com/wiki/Figure_eight"
 }
 
 data{
 	file = "glider.life",
+	image = "glider.bmp",
 	summary = "A glider (or featherweight spaceship) is the smallest, most common, and first-discovered spaceship. Its name is due in part to the fact that it is glide symmetric. It travels diagonally across the Life grid at a speed of c/4 and is often produced by randomly-generated starting patterns.",
 	source = "http://www.conwaylife.com/wiki/Glider"
 }
 
 data{
 	file = "rabbits.life",
+	image = "rabbits.bmp",
 	summary = "Rabbits is a methuselah that was found by Andrew Trevorrow in 1986. It is generation 1 of bunnies.",
 	source = "http://www.conwaylife.com/wiki/Rabbits"
 }
 
 data{
 	file = "heavySpaceship.life",
+	image = "heavySpaceship.bmp",
 	summary = "The heavyweight spaceship (or HWSS for short) is the fourth most common spaceship (after the glider, lightweight spaceship and middleweight spaceship). It was found by John Conway in 1970 and travels at a speed of c/2 orthogonally.",
 	source = "http://www.conwaylife.com/wiki/Heavyweight_spaceship"
 }
 
 data{
 	file = "octagon.life",
+	image = "octagon.bmp",
 	summary = "Octagon 2 was the first known period 5 oscillator, discovered in 1971 independently by Sol Goodman and Arthur Taber. The name is due to the latter.",
 	source = "http://www.conwaylife.com/wiki/Octagon_2"
 }
 
 data{
 	file = "pulsar.life",
+	image = "pulsar.bmp",
 	summary = "Pulsar is, despite its size, the fourth most common oscillator (and by far the most common of period greater than 2).",
 	source = "http://www.conwaylife.com/wiki/Pulsar"
 }
 
 data{
 	file = "bookend.life",
+	image = "bookend.bmp",
 	summary = "Bookend (or hook) is generation 1 of century and an induction coil.",
 	source = "http://www.conwaylife.com/wiki/Bookend"
 }
 
 data{
 	file = "blocker.life",
+	image = "blocker.bmp",
 	summary = "Blocker is a period 8 oscillator found by Robert Wainwright.",
 	source = "http://www.conwaylife.com/wiki/Blocker"
 }
 
-
 data{
 	file = "boatTie.life",
+	image = "boatTie.bmp",
 	summary = "Boat-tie (or bi-boat) is a small still life whose name is a pun on 'bow tie'. It is the twentieth most common still life, being less common than Integral sign but more common than snake.",
 	source = "http://www.conwaylife.com/wiki/Boat-tie"
 }
 
 data{
 	file = "25P3H1.life",
+	image = "25P3H1.bmp",
 	summary = "25P3H1V0.2 is an unnamed c/3 orthogonal spaceship discovered by Dean Hickerson in August 1989 and was the first c/3 spaceship to be discovered. In terms of its 25 cells, it is tied with 25P3H1V0.1 as the smallest c/3 spaceship, although unlike 25P3H1V0.1, it has a population of 25 in all of its phases, as well as a smaller bounding box.",
 	source = "http://www.conwaylife.com/wiki/25P3H1V0.2"
 }
