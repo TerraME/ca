ada48dc50f8896cf2a393dacdc932a5a1da5da2d
Pedro Andrade <pedro.andrade@inpe.br>
2016-11-16 15:24:11 -0200

    Tests updated to execute all source lines.

--- tests/Growth.lua
+++ tests/Growth.lua
@@ -4,7 +4,9 @@
 return{
 	Growth = function(unitTest)
 		local model = Growth{
-			finalTime = 5
+			finalTime = 5,
+			dim = 20,
+			probability = 0.4
 		}
 
 		unitTest:assertSnapshot(model.map, "Growth-map-1-begin.bmp")
--- tests/Utils.lua
+++ tests/Utils.lua
@@ -5,7 +5,12 @@ return{
 	countNeighbors = function(unitTest)
 		local l = Life{}
 
-		unitTest:assertEquals(countNeighbors(l.cs:sample(), "alive"), 0)
+		l.cs:synchronize()
+
+		forEachCell(l.cs, function(cell)
+			unitTest:assert(countNeighbors(cell, "alive") <= 8)
+		end)
+
 		unitTest:assertEquals(countNeighbors(l.cs:sample()), 8)
 	end
 }
