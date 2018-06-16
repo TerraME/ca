1896de9016047c0730538ad066504bdf199e3f2a
Matheus C. Zaglia <mzaglia@gmail.com>
2018-06-16 11:37:07 -0300

    Adding an Influenza model

---
+++ tests/Influenza.lua
@@ -0,0 +1,16 @@
+-- Test file for Influenza.lua
+-- Author: Pedro R. Andrade
+
+return{
+	Influenza = function(unitTest)
+		local model = Influenza{}
+
+		unitTest:assertSnapshot(model.map, "Influenza-map-1-begin.bmp")
+
+		model:run()
+
+		unitTest:assertSnapshot(model.chart, "Influenza-chart-1.bmp")
+		unitTest:assertSnapshot(model.map, "Influenza-map-1-end.bmp")
+	end,
+}
+
